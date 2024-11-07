88763e338e4cf9457585a5f9f2d92945790df644
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-07 17:39:29 +0100

    Add game function

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// INIT game variables
-let humanScore = 0;
-let computerScore = 0;
-
 // SET computer choice to random choice of rock, paper or scissor
 function getComputerChoice() {
     switch (Math.floor(Math.random() * 3)) {
@@ -35,34 +31,52 @@ function getHumanChoice() {
     }
 }
 
-// Determine winner
-function playRound (humanChoice, computerChoice) {
-    let humanWin;
-    if (humanChoice == 'Rock' && computerChoice != 'Rock') {
-        computerChoice == 'Paper' ? humanWin = true : humanWin = false;
-    } else if (humanChoice == 'Paper' && computerChoice != 'Paper') {
-        computerChoice == 'Rock' ? humanWin = true : humanWin = false;
-    } else if (humanChoice == 'Scissors' && computerChoice != 'Scissors') {
-        computerChoice == 'Paper' ? humanWin = true : humanWin = false;
-    }
-    displayRoundWinner(humanWin, humanChoice, computerChoice);
-}
 
-// Display and track winner
-function displayRoundWinner(humanWin='Tie', humanChoice, computerChoice) {
-    if (humanWin == 'Tie') {
-        console.log('It\'s a tie! ');
-    } else if (humanWin) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore++;
+
+
+
+
+function playGame () {
+    // Init game variables
+    let games = 0;    
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound (humanChoice, computerChoice) {
+        // Init round variable and helper function
+        let humanWin;
+        function processRoundWinner(humanWin='Tie', humanChoice, computerChoice) {
+            if (humanWin == 'Tie') {
+                console.log('It\'s a tie! ');
+            } else if (humanWin) {
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            } else {
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+            }
+        }
+
+        if (humanChoice == 'Rock' && computerChoice != 'Rock') {
+            computerChoice == 'Scissors' ? humanWin = true : humanWin = false;
+        } else if (humanChoice == 'Paper' && computerChoice != 'Paper') {
+            computerChoice == 'Rock' ? humanWin = true : humanWin = false;
+        } else if (humanChoice == 'Scissors' && computerChoice != 'Scissors') {
+            computerChoice == 'Paper' ? humanWin = true : humanWin = false;
+        }
+        processRoundWinner(humanWin, humanChoice, computerChoice);
+    }
+
+    while (games < 5) {
+        playRound(getHumanChoice(), getComputerChoice())
+        games++;
+    } 
+
+    if (humanScore > computerScore) {
+        console.log('You\'ve won the game!' + `\nYou ${humanScore} - ${computerScore} computer`);
+    } else if (computerScore > humanScore) {
+        console.log('The computer won the game!' + `\nYou ${humanScore} - ${computerScore} computer`);
     } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore++;
+        console.log('The game ended in a tie!' + `\nYou ${humanScore} - ${computerScore} computer`);
     }
 }
-
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
