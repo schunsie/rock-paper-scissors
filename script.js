20ed2f3a97ccee34d97f0038685856f37d55c657
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-07 13:00:23 +0100

    Add function to get computer pick at random

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello, world!");
+
+// SET computer choice to random choice of rock, paper or scissor
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+    }
+}
