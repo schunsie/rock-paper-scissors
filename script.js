e80d3bc3216cdd5209e9b39c61653c88e4d3c9b3
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-07 13:11:22 +0100

    Add function to get choice from user

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ function getComputerChoice() {
             return 'Scissors';
     }
 }
+
+// GET choice between rock, paper or scissors from human
+function getHumanChoice() {
+    while (true) {
+        try {
+            let choice = prompt('Rock, paper or scissors?').toLowerCase()
+            switch (choice) {
+                case 'rock':
+                    return 'Rock';
+                case 'paper':
+                    return 'Paper';
+                case 'scissors':
+                    return 'Scissors';
+                default:
+                    console.error(`"${choice}" is not a valid choice.`)
+            }
+        } catch (e) {
+            console.error('Choice cannot remain empty')
+        }
+    }
+}
+
