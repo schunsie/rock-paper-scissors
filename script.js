49ae3144b92607b4ede3d6e4071d22f5586b98f4
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-11 19:04:34 +0100

    Finalize playRound + helper functions

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ let computerScore = 0;
 
 buttons.addEventListener('click', (e) => {
     let target = e.target;
-    let choice = target.classList[0];
+    let playerChoice = target.classList[0];
     target.classList.toggle('active')
+    setTimeout(() => {
+        target.classList.toggle('active');
+    }, 1000);
 
 
-    if (validateChoice(choice)) {
-        playRound(choice);
+    if (validateChoice(playerChoice)) {
+        playRound(playerChoice);
     }
 });
 
@@ -21,22 +24,54 @@ function validateChoice(choice) {
     return false;
 }
 
-function playRound(choice) {
+function playRound(playerChoice) {
     const result = document.querySelector('#result');
     result.textContent = '';
     const computerChoice = getComputerChoice();
 
-    switch (determineWinner(choice, computerChoice)) {
+    switch (determineWinner(playerChoice, computerChoice)) {
         case 'player':
-            
+            result.textContent = `You win! ${playerChoice} beats ${computerChoice}`;
+            playerScore += 1;
+            break;
+        case 'computer':
+            result.textContent = `Computer wins! ${computerChoice} beats ${playerChoice}`;
+            computerScore += 1;
+            break;
+        case 'tie':
+            result.textContent = `It's a tie!`;
+            break;
     }
+
+    const score = document.querySelector('#score');
+    score.textContent = `${playerScore} - ${computerScore}`;
+
 }
 
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const index = Math.floor(Math.random() * 3);
-    return choices[index];
+    const computerChoice = choices[index];
+    
+    const choiceIcon = document.querySelector(`.computer-options .${computerChoice}`);
+    choiceIcon.classList.toggle('activeRed')
+    setTimeout(() => {
+        choiceIcon.classList.toggle('activeRed');
+    }, 1000);
+    
+    return computerChoice;
+    
 }
 
-function determineWinner(choice, computerChoice) {
+function determineWinner(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) return 'tie';
+    
+    switch (playerChoice) {
+        case 'rock':
+            return computerChoice == 'scissors' ? 'player' : 'computer';
+        case 'paper':
+            return computerChoice == 'rock' ? 'player' : 'computer';
+        case 'scissors':
+            return computerChoice == 'paper' ? 'player' : 'computer';
+    }
 }
